7f3ef2bff994edffaf1b51866bccdf7c7d32aab5
Eva Rodriguez <rodriguezmontesinoseva@gmail.com>
2019-05-24 15:36:27 +0200

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		// const headerIndex = (
+		// 	<header>
+
+		// 	</header>
+		// );
+
+		// const pageIndex = (
+		// 	<div className="app-container">
+		// 		{headerIndex}
+		// 	</div>
+		// );
+
+		// return pageIndex; //aqui sin paréntesis porque es un solo elemento
+		return (
+			<h1>soy APP</h1>
+		)
+
+	}
+}
+
+export default App;
+
